00e279e1d06db78b6c3b179b9402d9ac08900882
fushihui <444212235@qq.com>
2017-09-26 15:49:53 +0800

    CC手机版-支付

---
+++ protected/biz/pay/weixin/WxPay.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onwer
+ * Date: 2017/9/26
+ * Time: 15:40
+ */
+
+namespace biz\pay\weixin;
+
+require_once __DIR__ . "/lib/WxPay.Api.php";
+require_once __DIR__ . "/example/WxPay.JsApiPay.php";
+use JsApiPay;
+use WxPayApi;
+use WxPayConfig;
+use WxPayUnifiedOrder;
+
+class WxPay
+{
+    public static function instance()
+    {
+        return new self();
+    }
+
+    public function getJsApiParameters()
+    {
+
+//①、获取用户openid
+        $tools = new JsApiPay();
+        $openId = $tools->GetOpenid();
+
+//②、统一下单
+        $input = new WxPayUnifiedOrder();
+        $input->SetBody("test");
+        $input->SetAttach("test");
+        $input->SetOut_trade_no(WxPayConfig::getMCHID() . date("YmdHis"));
+        $input->SetTotal_fee("1");
+        $input->SetTime_start(date("YmdHis"));
+        $input->SetTime_expire(date("YmdHis", time() + 600));
+        $input->SetGoods_tag("test");
+        $input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+        $input->SetTrade_type("JSAPI");
+        $input->SetOpenid($openId);
+
+        $order = WxPayApi::unifiedOrder($input);
+
+        return $tools->GetJsApiParameters($order);
+    }
+}
