12e42bdcc6ea8ea89914ef59f9cb146e69101c91
f <444212235@qq.com>
2017-11-07 23:52:14 +0800

    11

--- protected/module/layouts/mobile.php
+++ protected/module/layouts/mobile.php
@@ -20,7 +20,7 @@
                 '/public/biz/swiper/css/swiper.min.css',
             '/public/biz/swiper/js/swiper.min.js',
                 '/public/biz/swipeSlide/js/swipeSlide.min.js',
-                '/public/biz/wx/common/css/style.css?12',
+                '/public/biz/wx/common/css/style.css?13',
                 '/public/biz/wx/common/js/script.js?9',
 
             ]
