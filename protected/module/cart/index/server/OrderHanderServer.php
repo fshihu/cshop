9ac9d7067feb03038dbf4223904ea6dfe9b56ffe
f <444212235@qq.com>
2018-12-25 23:27:29 +0800

    1

--- protected/module/cart/index/server/OrderHanderServer.php
+++ protected/module/cart/index/server/OrderHanderServer.php
@@ -15,6 +15,7 @@ use CC\db\base\update\UpdateModel;
 use module\groupon\server\GroupOneServer;
 use module\member\gold\server\GoldServer;
 use module\member\gold\server\UserGoldRecordServer;
+use module\member\index\server\UserLevelServer;
 use module\member\index\UserServer;
 use module\member\level\server\UserLevelOrderServer;
 use module\member\money\server\MoneyServer;
@@ -78,6 +79,40 @@ class OrderHanderServer
             ))->execute();
 
             MoneyServer::handlerOrder($order);
+        }
+        if($order['order_prom_type'] == PromTypeEnum::NORMAL){
+            $list = ListModel::make('order_goods')->addColumnsCondition(array(
+                'order_id' => $order['order_id']
+            ))->leftJoin('goods','g','t.goods_id = g.goods_id')
+                ->leftJoin('users','u','g.admin_uid = u.admin_uid')->select('t.*,u.merchant_ratio,u.user_id admin_to_uid,u.admin_uid')->execute();
+            $type_name = '';
+            foreach ($list as $item) {
+                if($item['card_discount_price'] > 0){
+                    $user = UserServer::getUser($order['user_id']);
+
+                    if(UserLevelServer::isGoldedCrad($user)){
+                        $type_name = '金卡';
+                    }
+                    if(UserLevelServer::isBlackCrad($user)){
+                        $type_name = '黑卡';
+                    }
+                    UserGoldRecordServer::addGold($user['user_id'],UserGoldRecordServer::TYPE_ORDER_CART_DISCOUNT_PRICE,$item['card_discount_price'],'订单'.$type_name.'返现',$item['rec_id']);
+
+                }
+            }
+
+        }
+        if($order['order_prom_type'] == PromTypeEnum::NORMAL) {
+            $get_gold = (int)($order['order_amount'] * GoldServer::getGoldRatio());
+            if ($get_gold > 0) {
+                UserGoldRecordServer::addGold($order['user_id'], UserGoldRecordServer::TYPE_BUY_GOODS_GET, $get_gold, '购买商品获得积分', $order['order_id']);
+            }
+            UpdateModel::make('order')->addData(array(
+                'get_gold' => $get_gold,
+            ))->addColumnsCondition(array(
+                'order_id' => $order['order_id'],
+            ))->execute();
+
         }
         if($order['order_prom_type'] == PromTypeEnum::GROUP_OPNE || $order['order_prom_type'] == PromTypeEnum::GROUP_JOIN
             || $order['order_prom_type'] == PromTypeEnum::GROUP_OWN_OPEN
--- protected/module/member/order/MemberOrderCommentWxAction.php
+++ protected/module/member/order/MemberOrderCommentWxAction.php
@@ -50,28 +50,6 @@ class MemberOrderCommentWxAction extends \CAction
                     'rating' => $request->getParams('rating'),
                     'add_time' => time(),
                 ))->execute();
-                if($item['card_discount_price'] > 0 && $order['order_prom_type'] == PromTypeEnum::NORMAL){
-                    $type_name = '';
-                    if(UserLevelServer::isGoldedCrad($user)){
-                        $type_name = '金卡';
-                    }
-                    if(UserLevelServer::isBlackCrad($user)){
-                        $type_name = '黑卡';
-                    }
-                    UserGoldRecordServer::addGold($user['user_id'],UserGoldRecordServer::TYPE_ORDER_CART_DISCOUNT_PRICE,$item['card_discount_price'],'订单'.$type_name.'返现',$item['rec_id']);
-                }
-            }
-            if($order['order_prom_type'] == PromTypeEnum::NORMAL) {
-                $get_gold = (int)($order['order_amount'] * GoldServer::getGoldRatio());
-                if ($get_gold > 0) {
-                    UserGoldRecordServer::addGold($order['user_id'], UserGoldRecordServer::TYPE_BUY_GOODS_GET, $get_gold, '购买商品获得积分', $order['order_id']);
-                }
-                UpdateModel::make('order')->addData(array(
-                    'get_gold' => $get_gold,
-                ))->addColumnsCondition(array(
-                    'order_id' => $order['order_id'],
-                ))->execute();
-
             }
             OrderStatusServer::instance($order['order_id'])->changeStatus(OrderStatusServer::TO_FINISH);
             return new \CJsonData();
